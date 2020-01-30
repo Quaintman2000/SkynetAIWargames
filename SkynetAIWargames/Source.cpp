e8bf54d4d220f48deed023bfb415fb5c8700fefa
Alec <Quaintman2000@gmail.com>
2020-01-29 21:18:49 -0700

    Added the grids to display

--- SkynetAIWargames/Source.cpp
+++ SkynetAIWargames/Source.cpp
@@ -5,7 +5,11 @@
 
 using namespace std;
 
-
+string humanArray[64];
+string binaryArray[64];
+string linearArray[64];
+string randomArray[64];
+string disp;
 
 int targetLocationPrediction;
 int searchGridHighNumber = 64;
@@ -20,9 +24,48 @@ bool algorSolved = false;
 bool linSolved = false;
 bool ranSolved = false;
 
+//function to display the grid
+ string displayGrid(string myArray[]) {
+	
+	string str = "";
+	// displays grid
+	for (int i = 0; i < 64; i++)
+	{
+		//Appropiately spaces & 0's for nums 1-8 for neatness then then skips to i=7 since it did them all.
+		if ((i + 1) < 8)
+		{
+			str += " " + myArray[i] + "  " + myArray[i + 1] + "  " + myArray[i + 2] + "  "
+				+ myArray[i + 3] + "  " + myArray[i + 4] + "  " + myArray[i + 5] + "  " + myArray[i + 6]
+				+ "  ";
+			i += 6;
+		}
+		//adds a space & a 0 infront of the 9
+		else if ((i + 1) == 9) {
+			str += " " + myArray[i] + " ";
+		}
+		//any number divisible by 8 makes a new line
+		else if ((i + 1) % 8 == 0)
+		{
+			str += myArray[i] + "\n";
+		}
+		else
+		{
+			str += myArray[i] + " ";
+		}
+	}
+	return str;
+}
 
 int main()
 {
+	//fills in the arrays
+	for (int i = 1; i < 64 + 1; i++) 
+	{
+		humanArray[i - 1] = to_string(i);
+		binaryArray[i - 1] = to_string(i);
+		linearArray[i - 1] = to_string(i);
+		randomArray[i - 1] = to_string(i);
+	}
 	// setting the seed for the random num generator so it's different each run
 	srand(static_cast<unsigned int>(time(0)));
 
@@ -30,28 +73,33 @@ int main()
 	int answer = rand() % 64 + 1;
 
 
-	cout << "Sending search party...\n\n";
+	cout << "We've recieved intel that the enemy is within grids 1-64, find them!\n\n";
 
 
 	cout << "----------------------------------------------------------------------------\n" << endl;
+	cout << "Enter a number betwen 1-64 to search..." << endl;
 	//Human
 	while (humanSolved == false)
 	{
 		//Human
 		
-		cout << "Human Guess..." << endl;
+		cout << "Enter the number..." << endl;
 		cin >> humanGuess;
 		//human guesses too low
 		if (humanGuess < answer)
 		{
 			cout << "Too low. Try again" << endl;
 			guesses++;
+			//x's out the wrong answer on the human grid
+			humanArray[humanGuess - 1] = " x";
 		}
 		//human guesses too high
 		else if (humanGuess > answer)
 		{
 			cout << "Too high. Try again" << endl;
 			guesses++;
+			//x's out the wrong answer on the human grid
+			humanArray[humanGuess - 1] = " x";
 		}
 		//human guesses correctly
 		else
@@ -59,10 +107,14 @@ int main()
 			guesses++;
 			cout << "Target located! The Humans found the target at "<<answer <<" in " << guesses<<" guesses."<< endl;
 			humanSolved = true;
+			cout << "----------------------------------------------------------------------------\n" << endl;
 		}
-		cout << "----------------------------------------------------------------------------\n" << endl;
+		
 	}
 
+	cout << "\n Human's grid \n";
+	//displays human grid
+	cout << disp << displayGrid(humanArray) << endl;
 
 	guesses = 0;
 	//Algorithm Ai
@@ -77,28 +129,38 @@ int main()
 			guesses++;
 			cout << "Target located! The Binary Ai found the target at " << answer << " in " << guesses << " guesses." << endl;
 			algorSolved = true;
-
+			cout << "----------------------------------------------------------------------------\n" << endl;
 		
 		}
 		//If the search 'overshot'
 		else if (targetLocationPrediction > answer)
 		{
 			guesses++;
-			cout << "Target not found at this location... " << targetLocationPrediction << " ...overshot. Rerouting search.\n" << endl;
+			//cout << "Target not found at this location... " << targetLocationPrediction << " ...overshot. Rerouting search.\n" << endl;
 			searchGridHighNumber = targetLocationPrediction;
-
-			
+			for (int i = targetLocationPrediction; i <= 64; i++)
+			{
+				binaryArray[i - 1] = " x";
+			}
 		}
 		// the search 'undershot'
 		else if (targetLocationPrediction < answer)
 		{
 			guesses++;
-			cout << "Target not found at this location... " << targetLocationPrediction << " ...undershot. Rerouting search.\n" << endl;
+			//cout << "Target not found at this location... " << targetLocationPrediction << " ...undershot. Rerouting search.\n" << endl;
 			searchGridLowNumber = targetLocationPrediction;
-
+			for (int i = 1; i <= targetLocationPrediction; i++)
+			{
+				binaryArray[i - 1] = " x";
+			}
 		}
-		cout << "----------------------------------------------------------------------------\n" << endl;
+		
 	}
+
+	cout << "\n Binary Ai's grid \n";
+	//displays binary grid
+	cout << disp << displayGrid(binaryArray) << endl;
+
 	guesses = 0;
 	// linear Ai
 	while (linSolved == false)
@@ -108,15 +170,19 @@ int main()
 		if (linearAiGuess > answer)
 		{
 			guesses++;
-			cout << "Linear Ai's guess of " << linearAiGuess << " was too high." << endl;
+		//	cout << "Linear Ai's guess of " << linearAiGuess << " was too high." << endl;
 			linearAiGuess--;
+			//x's out the wrong answer on the linear AI grid
+			linearArray[linearAiGuess - 1] = "x";
 		}
 		//Linear Ai guesses too low
 		else if (linearAiGuess < answer)
 		{
 			guesses++;
-			cout << "Linear Ai's guess of " << linearAiGuess << " was too low." << endl;
+			//cout << "Linear Ai's guess of " << linearAiGuess << " was too low." << endl;
 			linearAiGuess++;
+			//x's out the wrong answer on the linear AI grid
+			linearArray[linearAiGuess - 1] = "x";
 		}
 		//when found
 		else
@@ -124,9 +190,14 @@ int main()
 			guesses++;
 			cout << "Target located! The Linear Ai found the target at " << answer << " in " << guesses << " guesses." << endl;
 			linSolved = true;
+			cout << "----------------------------------------------------------------------------\n" << endl;
 		}
-		cout << "----------------------------------------------------------------------------\n" << endl;
+		
 	}
+
+	cout << "\n Linear Ai's grid \n";
+	//displays linear grid
+	cout << disp << displayGrid(linearArray) << endl;
 
 	guesses = 0;
 	//Rand Ai
@@ -137,14 +208,18 @@ int main()
 		if (randAiGuess > answer)
 		{
 			guesses++;
-			cout << "Random Ai's guess of " << randAiGuess << " was too high." << endl;
+			//x's out the wrong answer on the random AI grid
+			randomArray[randAiGuess- 1] = "x";
+			//cout << "Random Ai's guess of " << randAiGuess << " was too high." << endl;
 			randAiGuess = rand() % 64 + 1;
 		}
 		//Random Ai guess too low
 		else if (randAiGuess < answer)
 		{
 			guesses++;
-			cout << "Random Ai's guess of " << randAiGuess << " was too low." << endl;
+			//x's out the wrong answer on the random AI grid
+			randomArray[randAiGuess - 1] = "x";
+			//cout << "Random Ai's guess of " << randAiGuess << " was too low." << endl;
 			randAiGuess = rand() % 64 + 1;
 		}
 		////Random Ai guess correctly
@@ -153,9 +228,14 @@ int main()
 			guesses++;
 			cout << "Target located! The Random Ai found the target at " << answer << " in " << guesses << " guesses." << endl;
 			ranSolved = true;
+			cout << "----------------------------------------------------------------------------\n" << endl;
 		}
-		cout << "----------------------------------------------------------------------------\n" << endl;
+		
 	}
+
+	cout << "\n Random Ai's grid \n";
+	//displays random ai's gird
+	cout << disp << displayGrid(randomArray) << endl;
 }
 
 
